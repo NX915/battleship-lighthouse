674512cf85ec8d30f842fce4d891f33b5eb33f9e
Sean Ni <nixiang915@hotmail.com>
2020-08-23 16:25:38 +0000

    moved printBoard into board.js

--- battleship.js
+++ battleship.js
@@ -1,22 +1,9 @@
 const board = require('./board');
 const ship = require('./ship');
 
-const printBoard = function(boardArr) {
-  let rowTxt = '', boardTxt = '';
-  for (let i = 0; i < boardArr.length; i++) {
-    rowTxt = '';
-    for (let j = 0; j < boardArr.length; j++) {
-      rowTxt += boardArr[i][j];
-    }
-    boardTxt += rowTxt + '\n';
-  }
-  console.log(boardTxt);
-  return boardTxt;
-};
-
 const battleship = function() {
   const playerBoardObj = ship.placeShips(board.initBoard());
-  printBoard(playerBoardObj.board);
+  board.printBoard(playerBoardObj.board);
 };
 
 battleship();
--- board.js
+++ board.js
@@ -9,6 +9,18 @@ const board = {
     }
     return boardArr;
   },
+  printBoard: function(boardArr) {
+    let rowTxt = '', boardTxt = '';
+    for (let i = 0; i < boardArr.length; i++) {
+      rowTxt = '';
+      for (let j = 0; j < boardArr.length; j++) {
+        rowTxt += boardArr[i][j];
+      }
+      boardTxt += rowTxt + '\n';
+    }
+    console.log(boardTxt);
+    return boardTxt;
+  },
 };
 
 module.exports = board;
