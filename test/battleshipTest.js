68761091cdd084f39b12360c1655b5a330ff06fb
Sean Ni <nixiang915@hotmail.com>
2020-08-23 17:59:08 +0000

    test now test default symbols instead of hardcoded symbols

--- test/battleshipTest.js
+++ test/battleshipTest.js
@@ -3,6 +3,7 @@ const assert = chai.assert;
 const expect = chai.expect;
 const board = require('../board');
 const ship = require('../ship');
+const waterSym = board.sym.water;
 
 describe("initBoard function create an nested array representing a 2D game board", function() {
   it("Make a board array with 10 elements by default", () => {
@@ -11,8 +12,8 @@ describe("initBoard function create an nested array representing a 2D game board
   it("Make a board array with 10 nested elements by default", () => {
     assert.equal(board.initBoard()[0].length, 10);
   });
-  it("Make a board array with nested elements of 🟦 default", () => {
-    assert.equal(board.initBoard()[0][0], '🟦');
+  it(`Make a board array with nested elements of ${waterSym} default`, () => {
+    assert.equal(board.initBoard()[0][0], waterSym);
   });
   it("Make a board array with 15 elements as first parameter", () => {
     assert.equal(board.initBoard(15).length, 15);
@@ -26,49 +27,49 @@ describe("initBoard function create an nested array representing a 2D game board
 });
 describe("randomCoord return an object {x:y} coordinate within the argument board dimention", function() {
   it("return x between 0 and 1 with board size 1x1", () => {
-    let testBoard = [['🟦']];
+    let testBoard = [[waterSym]];
     expect(ship.randomCoord(testBoard).x).to.within(0, 1);
   });
   it("return y between 0 and 1 with board size 1x1", () => {
-    let testBoard = [['🟦']];
+    let testBoard = [[waterSym]];
     expect(ship.randomCoord(testBoard).y).to.within(0, 1);
   });
   it("return x between 0 and 2 with board size 2x2", () => {
-    let testBoard = [['🟦', '🟦'], ['🟦', '🟦']];
+    let testBoard = [[waterSym, waterSym], [waterSym, waterSym]];
     expect(ship.randomCoord(testBoard).x).to.within(0, 2);
   });
   it("return y between 0 and 2 with board size 2x2", () => {
-    let testBoard = [['🟦', '🟦'], ['🟦', '🟦']];
+    let testBoard = [[waterSym, waterSym], [waterSym, waterSym]];
     expect(ship.randomCoord(testBoard).y).to.within(0, 2);
   });
   it("return x between 0 and 5 with board size 5x1", () => {
-    let testBoard = [['🟦', '🟦', '🟦', '🟦', '🟦']];
+    let testBoard = [[waterSym, waterSym, waterSym, waterSym, waterSym]];
     expect(ship.randomCoord(testBoard).x).to.within(0, 5);
   });
   it("return y between 0 and 1 with board size 5x1", () => {
-    let testBoard = [['🟦', '🟦', '🟦', '🟦', '🟦']];
+    let testBoard = [[waterSym, waterSym, waterSym, waterSym, waterSym]];
     expect(ship.randomCoord(testBoard).y).to.within(0, 1);
   });
   it("return x between 0 and 1 with board size 1x5", () => {
-    let testBoard = [['🟦'], ['🟦'], ['🟦'], ['🟦'], ['🟦']];
+    let testBoard = [[waterSym], [waterSym], [waterSym], [waterSym], [waterSym]];
     expect(ship.randomCoord(testBoard).x).to.within(0, 1);
   });
   it("return y between 0 and 5 with board size 1x5", () => {
-    let testBoard = [['🟦'], ['🟦'], ['🟦'], ['🟦'], ['🟦']];
+    let testBoard = [[waterSym], [waterSym], [waterSym], [waterSym], [waterSym]];
     expect(ship.randomCoord(testBoard).y).to.within(0, 5);
   });
 });
 describe("placeShip will place ships into a random spot on given board if space allows", function() {
-  it("Will not place a default destroyer size 2x1 (🔸) on a 1x1 empty default board (🟦)", () => {
-    let testBoard = ['🟦'];
-    expect(ship.placeShips(testBoard, {destroyer: 1}).board).to.eql(['🟦']);
+  it(`Will not place a default destroyer size 2x1 (🔸) on a 1x1 empty default board (${waterSym})`, () => {
+    let testBoard = [waterSym];
+    expect(ship.placeShips(testBoard, {destroyer: 1}).board).to.eql([waterSym]);
   });
-  it("Will place a default destroyer size 2x1 (🔸) on a 2x1 empty default board (🟦)", () => {
-    let testBoard = [['🟦', '🟦']];
+  it(`Will place a default destroyer size 2x1 (🔸) on a 2x1 empty default board (${waterSym})`, () => {
+    let testBoard = [[waterSym, waterSym]];
     expect(ship.placeShips(testBoard, {destroyer: 1}).board).to.eql([['🔸', '🔸']]);
   });
-  it("Will place a default destroyer size 2x1 (🔸) on a 1x2 empty default board (🟦)", () => {
-    let testBoard = [['🟦'], ['🟦']];
+  it(`Will place a default destroyer size 2x1 (🔸) on a 1x2 empty default board (${waterSym})`, () => {
+    let testBoard = [[waterSym], [waterSym]];
     expect(ship.placeShips(testBoard, {destroyer: 1}).board).to.eql([['🔸'], ['🔸']]);
   });
 });
